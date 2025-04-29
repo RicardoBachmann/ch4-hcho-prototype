0ebe65538f369bbeec96d3333d18d25fd2973dd6
RicardoBachmann <ricardo.bachmann@icloud.com>
2025-04-29 11:22:05 +0200

    Add vite proxy configuration for N2YO API resolve CORS issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api/n2yo": {
+        target: "https://api.n2yo.com/rest/v1/satellite",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/n2yo/, ""),
+      },
+    },
+  },
+});
