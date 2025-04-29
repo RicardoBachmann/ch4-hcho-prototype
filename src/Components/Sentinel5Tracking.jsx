89ff0305c053ea67af422d4d0b52326874915cb7
RicardoBachmann <ricardo.bachmann@icloud.com>
2025-04-29 12:16:23 +0200

    Successfully fetch Sentinal-5 satellite postion data from N2YO API

--- src/Components/Sentinel5Tracking.jsx
+++ src/Components/Sentinel5Tracking.jsx
@@ -1,22 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Sentinel5Tracking() {
+  const [satelliteData, setSatelliteData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    console.log("API Key:", import.meta.env.VITE_N2YO_API_KEY);
-    try {
-      fetch(
-        `https://api.n2yo.com/rest/v1/satellite/positions/42969/41.702/-76.014/0/2/&apiKey=${
-          import.meta.env.VITE_N2YO_API_KEY
-        }`
-      )
-        .then((res) => res.json())
-        .then((data) => console.log("Sentinel-5 data:", data))
-        .catch((error) =>
-          console.error("Error in fetch promise chain:", error)
+    const getSentinel5Data = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `/api/n2yo/positions/42969/41.702/-76.014/0/2?apiKey=${
+            import.meta.env.VITE_N2YO_API_KEY
+          }`
         );
-    } catch (error) {
-      console.error("Error Fetching Sentinel-5 data:", error);
-    }
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Sentinel-5 data:", data);
+        setSatelliteData(data);
+      } catch (error) {
+        console.error("Error fetching sentinel5 data:", error);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getSentinel5Data();
   }, []);
   return null;
 }
