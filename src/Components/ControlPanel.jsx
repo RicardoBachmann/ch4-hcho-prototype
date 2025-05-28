e5b4c9e66ab9946f9904b8d8617404701da8633a
RicardoBachmann <ricardo.bachmann@icloud.com>
2025-05-28 11:34:58 +0200

    feat: display STAC metadata with defensive programming in ControlPanel

--- src/Components/ControlPanel.jsx
+++ src/Components/ControlPanel.jsx
@@ -16,16 +16,69 @@ export default function ControlPanel({
         zIndex: 1000,
       }}
     >
-      <h3>Sentinel 5 Position:</h3>
-      <ul>
-        <li>Latitude:{sentinel5Position.latitude.toFixed(3)}</li>
-        <li>Longitude:{sentinel5Position.longitude.toFixed(3)}</li>
-      </ul>
-      {sentinelData.formaldehyde && (
-        <p>
-          Date time:{sentinelData.formaldehyde.features[0].properties.datetime}
-        </p>
+      <h2>Sentinel-5 Data:</h2>
+      {sentinelData && sentinelData.formaldehyde && (
+        <label>
+          {" "}
+          Current S5 Position:
+          <ul style={{ listStyle: "none5" }}>
+            <li>Latitude:{sentinel5Position.latitude.toFixed(3)}</li>
+            <li>Longitude:{sentinel5Position.longitude.toFixed(3)}</li>
+          </ul>
+          <p>
+            Instruments:{" "}
+            {sentinelData.formaldehyde.features[0].properties.instruments}
+          </p>
+          <p>
+            Processing Level:{" "}
+            {
+              sentinelData.formaldehyde.features[0].properties[
+                "processing:level"
+              ]
+            }
+          </p>
+          <p>
+            Spatial Resolution:
+            {
+              sentinelData.formaldehyde.features[0].properties[
+                "s5p:spatial_resolution"
+              ]
+            }
+          </p>
+        </label>
       )}
+      <hr />
+      <h4>Formaldehyde HCHO</h4>
+      {sentinelData.formaldehyde &&
+        (() => {
+          const formatDate = (dateString) => new Date(dateString).toUTCString();
+          const properties = sentinelData.formaldehyde.features[0].properties;
+
+          return (
+            <div>
+              <p>
+                Date Time: <br />
+                {formatDate(properties.datetime)}
+              </p>
+              <p>
+                Start: <br />
+                {formatDate(properties.start_datetime)}
+              </p>
+              <p>
+                End: <br />
+                {formatDate(properties.end_datetime)}
+              </p>
+              <p>
+                Create: <br />
+                {formatDate(properties.created)}
+              </p>
+              <p>
+                Update: <br />
+                {formatDate(properties.updated)}
+              </p>
+            </div>
+          );
+        })()}
     </div>
   );
 }
